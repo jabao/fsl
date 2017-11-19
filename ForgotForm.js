7f6f729a1bbb7a46251b112d4909f28f6411ad62
XuanZzz <shinezhux@gmail.com>
2017-11-18 16:06:45 -0800

    Forgot password added

---
+++ ForgotForm.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  StatusBar
+} from 'react-native';
+
+import { Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
+
+
+
+export class ForgotForm extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+		};
+		this.submit=this.submit.bind(this);
+	}
+
+	submit() {
+		firebase.auth().sendPasswordResetEmail(this.state.email)
+			.then(function() {
+				alert("Password Reset Email Sent!");
+				Actions.pop();
+			})
+			.catch(function(error) {
+				alert(error.message);
+			    this.setState({
+			    	email: '',
+			    });
+			}.bind(this));
+	}
+
+
+	render() {
+		return (
+			<View style = {styles.container}>
+			<StatusBar 
+				barStyle="light-content"
+				/>
+				<TextInput 
+					placeholder="username or email"
+					placeholderTextColor='rgba(255,255,255,0.7)'
+					returnKeyType="next"
+					keyboardType="email-address"
+					autoCapitalize="none"
+					autoCorrect={false}
+					style={styles.input}
+					onChangeText={(email) => this.setState({email})}
+					value={this.state.email}
+					/>
+				<TouchableOpacity style={styles.buttonContainer}>
+					<Text 
+						style={styles.buttonText}
+						onPress={this.submit}
+						>
+						Submit
+					</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+	},
+	input: {
+		height: 40,
+		backgroundColor: 'rgba(255,255,255,0.2)',
+		marginBottom: 10,
+		color: '#FFF',
+		paddingHorizontal: 10,
+	},
+	buttonContainer: {
+		backgroundColor: '#2980b9',
+		paddingVertical: 15,
+	},
+	buttonText: {
+		textAlign: 'center',
+		color: '#FFFFFF',
+		fontWeight: '700'
+	}
+});
--- Routes.js
+++ Routes.js
@@ -3,6 +3,7 @@ import {Router, Stack, Scene} from 'react-native-router-flux';
 
 import {Login} from './Login';
 import {Signup} from './Signup';
+import {Forgot} from './Forgot';
 import {Map} from './Map';
 
 
@@ -13,6 +14,7 @@ export class Routes extends Component {
 				<Stack key="root" hideNavBar={true}>
 					<Scene key="login" component={Login} title="Login"/>
 			    	<Scene key="signup" component={Signup} title="Signup"/>
+			    	<Scene key="forgot" component={Forgot} title="Forgot" />
 			    	<Scene key="map" component={Map} title="Map"/>
 			    </Stack>
 			</Router>
